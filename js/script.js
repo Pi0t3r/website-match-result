dd82729a95f54a5ffa58c2f62b5d50646c35cada
Piotr Kapuściński <kapuscinskipiotr5@gmail.com>
2022-11-07 21:02:06 +0100

    added section about other sports, toggling class between then and added, filter match

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const navigations = document.querySelectorAll(".navigation");
 const sports = document.querySelectorAll(".item");
+const btnFilter = document.querySelectorAll(".btn-filter");
 
 const resultsAndNewsToggleClass = (e) => {
   navigations.forEach((element) => {
@@ -21,4 +22,26 @@ const sportsToggleClass = (e) => {
 
 sports.forEach((el) => {
   el.addEventListener("click", sportsToggleClass);
+});
+
+const btnFilterTogglingClass = (e) => {
+  btnFilter.forEach((element) => {
+    element.classList.remove("active");
+  });
+  e.currentTarget.classList.add("active");
+};
+
+btnFilter.forEach((el) => {
+  el.addEventListener("click", btnFilterTogglingClass);
+});
+
+let picker = $("#test")
+  .pickadate({
+    format: "dd/mm",
+  })
+  .pickadate("picker");
+
+$("#previous-day,#next-day").click(function (e) {
+  e.preventDefault();
+  setInterval($(this).data('diff'))
 });

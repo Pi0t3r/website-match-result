579b866514b0fbc4fa3eaeee3004d845332a7d01
Piotr Kapuściński <kapuscinskipiotr5@gmail.com>
2022-11-08 18:56:50 +0100

    Added a div 'calendar', code optimization about toggling class 'active'

--- js/script.js
+++ js/script.js
@@ -2,46 +2,18 @@ const navigations = document.querySelectorAll(".navigation");
 const sports = document.querySelectorAll(".item");
 const btnFilter = document.querySelectorAll(".btn-filter");
 
-const resultsAndNewsToggleClass = (e) => {
-  navigations.forEach((element) => {
-    element.classList.remove("active");
-  });
-  e.currentTarget.classList.add("active");
-};
+const elements = [
+  document.querySelectorAll(".navigation"),
+  document.querySelectorAll(".item"),
+  document.querySelectorAll(".btn-filter"),
+];
 
-navigations.forEach((el) => {
-  el.addEventListener("click", resultsAndNewsToggleClass);
-});
-
-const sportsToggleClass = (e) => {
-  sports.forEach((element) => {
-    element.classList.remove("active");
-  });
-  e.currentTarget.classList.add("active");
-};
-
-sports.forEach((el) => {
-  el.addEventListener("click", sportsToggleClass);
-});
-
-const btnFilterTogglingClass = (e) => {
-  btnFilter.forEach((element) => {
-    element.classList.remove("active");
-  });
-  e.currentTarget.classList.add("active");
-};
-
-btnFilter.forEach((el) => {
-  el.addEventListener("click", btnFilterTogglingClass);
-});
-
-let picker = $("#test")
-  .pickadate({
-    format: "dd/mm",
-  })
-  .pickadate("picker");
-
-$("#previous-day,#next-day").click(function (e) {
-  e.preventDefault();
-  setInterval($(this).data('diff'))
+elements.forEach((items) => {
+  const toggle = (e) => {
+    items.forEach((element) => {
+      element.classList.remove("active");
+    });
+    e.currentTarget.classList.add("active");
+  };
+  items.forEach((item) => item.addEventListener("click", toggle));
 });
